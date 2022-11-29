801882ec6d5a04740482617d686fca3221a3eb50
tomoyo.tandokoro <tomoyo.tandokoro@rei-frontier.jp>
2022-11-30 07:39:01 +0900

    no message

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,37 +4,52 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   final title = 'Flutterサンプル';
-  final message = 'サンプル・メッセージ';
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      home: new MyHomePage(title: this.title, message: this.message),
+      home: new MyHomePage(title: this.title),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-  final String message;
+  MyHomePage({required this.title}) : super();
 
-  MyHomePage({required this.title, required this.message}) : super();
+  final String title;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late String _message;
+
+  @override
+  void initState() {
+    super.initState();
+    _message = 'Hello!';
+  }
+
+  void _setMessage() {
+    setState(() {
+      _message = 'タップしました！';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Text(
-        widget.message,
+        _message,
         style: TextStyle(fontSize: 32.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setMessage,
+        tooltip: 'set message.',
+        child: Icon(Icons.star),
       ),
     );
   }
